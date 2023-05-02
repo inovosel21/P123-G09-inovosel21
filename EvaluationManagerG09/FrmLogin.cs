558214ac5617121507d841cb9cfbca5c2516451f
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-02 14:57:49 +0200

    Vjezba3

--- EvaluationManagerG09/FrmLogin.cs
+++ EvaluationManagerG09/FrmLogin.cs
@@ -23,7 +23,9 @@ namespace EvaluationManagerG09 {
         }
 
         private void button1_Click(object sender, EventArgs e) {
-
+            if(txtUsername.Text == "") {
+                MessageBox.Show("Korisničko ime nije unešeno", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
